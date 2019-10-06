a30b57f227aa5d901209945d526824cde9866492
Bruno Sholla <bsholla@tetra.al>
2019-10-06 17:10:32 +0200

    Created Api call

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            user: null
+        }
+
+    }
+
+    componentDidMount() {
+        fetch('http://localhost:8080/hi')
+            .then(res => res.json())
+            .then((data) => {
+                this.setState({user: data})
+            })
+            .catch(console.log)
+    }
+
+    render() {
+       const {user}=this.state
+
+        return user!==null?(
+            <div className="card">
+                <div className="card-body">
+                    <h5 className="card-title">{`${user.firstname} ${user.lastname}`}</h5>
+                    <h6 className="card-subtitle mb-2 text-muted">{user.email}</h6>
+                    <p className="card-text">{user.address}</p>
+                </div>
+            </div>
+        ):(<div> No data</div>)
+    }
 }
 
 export default App;
